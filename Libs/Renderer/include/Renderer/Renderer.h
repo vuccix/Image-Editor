09f83322cd86777dfe2c5fa6e840f2f3782736a8
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-06-24 17:44:55 +0200

    add image uploading to Renderer

--- Libs/Renderer/include/Renderer/Renderer.h
+++ Libs/Renderer/include/Renderer/Renderer.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <Canvas/Image.h>
 #include <cstdint>
 
 class Renderer {
 public:
     Renderer();
    ~Renderer();
+
+    void upload(const Image& img) const;
+    uint32_t textureID() const;
 
 private:
     uint32_t m_texture = 0;
--- Libs/Renderer/src/Renderer.cpp
+++ Libs/Renderer/src/Renderer.cpp
@@ -17,3 +17,43 @@ Renderer::~Renderer() {
     if (m_texture != 0)
         glDeleteTextures(1, &m_texture);
 }
+
+uint32_t Renderer::textureID() const {
+    return m_texture;
+}
+
+void Renderer::upload(const Image& img) const {
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+
+    if (img.channels == 1) {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_RED);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ONE);
+    }
+    else {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
+    }
+
+    GLenum format; GLint internal;
+    switch (img.channels) {
+        case 3:  format = GL_RGB;  internal = GL_RGB8;  break;
+        case 4:  format = GL_RGBA; internal = GL_RGBA8; break;
+        default: format = GL_RED;  internal = GL_R8;    break;
+    }
+
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        internal,
+        img.width,
+        img.height,
+        0,
+        format,
+        GL_UNSIGNED_BYTE,
+        img.pixels.data()
+    );
+}
