972b02713975019227a16f7e0e0c95c6cd4b3c0f
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-07-06 16:26:24 +0200

    add resize and update methods to Texture

--- Libs/Renderer/include/Renderer/Texture.h
+++ Libs/Renderer/include/Renderer/Texture.h
@@ -10,16 +10,18 @@ public:
 
     Texture(const Texture&)            = delete;
     Texture& operator=(const Texture&) = delete;
-
-    Texture(Texture&& other) noexcept;
+    Texture(Texture&& other)            noexcept;
     Texture& operator=(Texture&& other) noexcept;
+
+    void resize(uint32_t width, uint32_t height);
+    void update(std::span<const uint8_t> rgbaPixels);
 
     uint32_t id()     const noexcept;
     uint32_t width()  const noexcept;
     uint32_t height() const noexcept;
 
 private:
-    uint32_t m_textureId = 0;
-    uint32_t m_width     = 0;
-    uint32_t m_height    = 0;
+    uint32_t m_texture = 0;
+    uint32_t m_width   = 0;
+    uint32_t m_height  = 0;
 };
--- Libs/Renderer/src/Texture.cpp
+++ Libs/Renderer/src/Texture.cpp
@@ -5,8 +5,8 @@
 Texture::Texture(const uint32_t width, const uint32_t height, const std::span<const uint8_t> rgbaPixels)
         : m_width(width), m_height(height) {
 
-    glGenTextures(1, &m_textureId);
-    glBindTexture(GL_TEXTURE_2D, m_textureId);
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -29,28 +29,64 @@ Texture::Texture(const uint32_t width, const uint32_t height, const std::span<co
 }
 
 Texture::~Texture() {
-    if (m_textureId != 0)
-        glDeleteTextures(1, &m_textureId);
+    if (m_texture != 0)
+        glDeleteTextures(1, &m_texture);
 }
 
 Texture::Texture(Texture&& other) noexcept
-        : m_textureId(std::exchange(other.m_textureId, 0)),
+        : m_texture(std::exchange(other.m_texture, 0)),
           m_width(std::exchange(other.m_width, 0)),
           m_height(std::exchange(other.m_height, 0)) {}
 
 Texture& Texture::operator=(Texture&& other) noexcept {
     if (this != &other) {
-        if (m_textureId != 0)
-            glDeleteTextures(1, &m_textureId);
+        if (m_texture != 0)
+            glDeleteTextures(1, &m_texture);
 
-        m_textureId = std::exchange(other.m_textureId, 0);
-        m_width     = std::exchange(other.m_width,     0);
-        m_height    = std::exchange(other.m_height,    0);
+        m_texture = std::exchange(other.m_texture, 0);
+        m_width   = std::exchange(other.m_width,   0);
+        m_height  = std::exchange(other.m_height,  0);
     }
 
     return *this;
 }
 
-uint32_t Texture::id()     const noexcept { return m_textureId; }
-uint32_t Texture::width()  const noexcept { return m_width;     }
-uint32_t Texture::height() const noexcept { return m_height;    }
+void Texture::resize(const uint32_t width, const uint32_t height) {
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+
+    // allocate memory on the GPU without uploading data
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA8,
+        static_cast<GLsizei>(width),
+        static_cast<GLsizei>(height),
+        0,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        nullptr
+    );
+
+    m_width  = width;
+    m_height = height;
+}
+
+void Texture::update(const std::span<const uint8_t> rgbaPixels) {
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+
+    // replace existing GPU pixels
+    glTexSubImage2D(
+        GL_TEXTURE_2D,
+        0,
+        0, 0,
+        static_cast<GLsizei>(m_width),
+        static_cast<GLsizei>(m_height),
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        rgbaPixels.data()
+    );
+}
+
+uint32_t Texture::id()     const noexcept { return m_texture; }
+uint32_t Texture::width()  const noexcept { return m_width;   }
+uint32_t Texture::height() const noexcept { return m_height;  }
