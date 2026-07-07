d8afb6c6462b5e37c48fe0c92ba862976d3d526a
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-07-07 15:45:54 +0200

    Update UIManager.cpp

--- App/View/UIManager.cpp
+++ App/View/UIManager.cpp
@@ -390,8 +390,8 @@ void UIManager::drawMenuBar(EditorState& state, Controller& controller, const st
 
         ui.menu("Filters", [&] {
             ui.menu("Adjust", [&] {
-                ui.item("Brightness...", [&] {});
-                ui.item("Contrast...", [&] {});
+                ui.item("Brightness...", [&] { exec(Cmd::brightness(25)); });
+                ui.item("Contrast...",   [&] { exec(Cmd::contrast(5.f));  });
                 ui.item("Saturation...", [&] {});
             });
 
