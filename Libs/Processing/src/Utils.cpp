377e3491f0cba2bb3db6070f891af0bb8f16e757
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-07-09 19:12:44 +0200

    refactor convolution

--- Libs/Processing/src/Utils.cpp
+++ Libs/Processing/src/Utils.cpp
@@ -5,25 +5,25 @@
 #include <vector>
 #include <omp.h>
 
-template <typename T>
-void Utils::convolution(Layer& image, std::mdspan<const T, std::dextents<size_t, 2>> kernel) {
-    std::vector result(image.width() * image.height(), Pixel{});
-
-    const auto rows    = static_cast<int32_t>(image.height());
-    const auto cols    = static_cast<int32_t>(image.width());
+template <typename T, typename U>
+std::vector<T> Utils::convolution(std::mdspan<const T, std::dextents<size_t, 2>> image,
+                                  std::mdspan<const U, std::dextents<size_t, 2>> kernel) {
+    const auto rows    = static_cast<int32_t>(image.extent(0));
+    const auto cols    = static_cast<int32_t>(image.extent(1));
     const auto kHeight = static_cast<int32_t>(kernel.extent(0));
     const auto kWidth  = static_cast<int32_t>(kernel.extent(1));
 
     const int32_t rW   = kWidth  / 2;
     const int32_t rH   = kHeight / 2;
 
+    std::vector<T> result(rows * cols, T{});
+
     auto index = [](const int32_t id, const int32_t max) -> int32_t {
         return (id < 0) ? -id - 1 : (id >= max) ? 2 * max - id - 1 : id;
     };
 
-    const auto pixels = image.pixels();
     auto sumKernel = [&](const int32_t x, const int32_t y) {
-        std::array<T, 3> sum = { 0, 0, 0 };
+        std::array<U, 3> sum = { 0, 0, 0 };
 
         for (int32_t ky = -rH; ky <= rH; ++ky) {
             const int32_t iy = index(y + ky, rows);
@@ -33,12 +33,17 @@ void Utils::convolution(Layer& image, std::mdspan<const T, std::dextents<size_t,
                 const int32_t ix = index(x + kx, cols);
                 const int32_t kj = kx + rW;
 
-                const T       v  = kernel[ki, kj];
-                const Pixel&  p  = pixels[iy, ix];
+                const U       v  = kernel[ki, kj];
+                const T&      p  = image[iy, ix];
 
-                sum[0] += p.r * v;
-                sum[1] += p.g * v;
-                sum[2] += p.b * v;
+                if constexpr (std::is_same_v<T, Pixel>) {
+                    sum[0] += p.r * v;
+                    sum[1] += p.g * v;
+                    sum[2] += p.b * v;
+                }
+                else {
+                    sum[0] += p * v;
+                }
             }
         }
 
@@ -50,21 +55,47 @@ void Utils::convolution(Layer& image, std::mdspan<const T, std::dextents<size_t,
     #pragma omp parallel for collapse(2)
     for (int32_t y = 0; y < rows; ++y) {
         for (int32_t x = 0; x < cols; ++x) {
-            const auto [sumR, sumG, sumB] = sumKernel(x, y);
-            constexpr T lo = 0, hi = 255;
+            if constexpr (std::is_same_v<T, Pixel>) {
+                const auto [sumR, sumG, sumB] = sumKernel(x, y);
+                constexpr U lo = 0, hi = 255;
 
-            newPixels[y, x] = {
-                .r = static_cast<uint8_t>(std::clamp(sumR, lo, hi)),
-                .g = static_cast<uint8_t>(std::clamp(sumG, lo, hi)),
-                .b = static_cast<uint8_t>(std::clamp(sumB, lo, hi)),
-                .a = pixels[y, x].a
-            };
+                newPixels[y, x] = {
+                    .r = static_cast<uint8_t>(std::clamp(sumR, lo, hi)),
+                    .g = static_cast<uint8_t>(std::clamp(sumG, lo, hi)),
+                    .b = static_cast<uint8_t>(std::clamp(sumB, lo, hi)),
+                    .a = image[y, x].a
+                };
+            }
+            else {
+                const auto sum  = sumKernel(x, y);
+                newPixels[y, x] = sum[0];
+            }
         }
     }
 
-    image.setData(std::move(result));
+    return result;
 }
 
-template void Utils::convolution<int32_t>(Layer& image, std::mdspan<const int32_t, std::dextents<size_t, 2>> kernel);
-template void Utils::convolution<float  >(Layer& image, std::mdspan<const float,   std::dextents<size_t, 2>> kernel);
-template void Utils::convolution<double >(Layer& image, std::mdspan<const double,  std::dextents<size_t, 2>> kernel);
+// explicit instantiation ----------------------------------------------------------------------------------------------
+
+namespace Utils {
+
+#define INSTANTIATE_CONVOLUTION(T, K)                                                          \
+    template std::vector<T> convolution<T, K>(std::mdspan<const T, std::dextents<size_t, 2>>,  \
+                                              std::mdspan<const K, std::dextents<size_t, 2>>); \
+
+#define INSTANTIATE_FOR_TYPE(T)         \
+    INSTANTIATE_CONVOLUTION(T, int32_t) \
+    INSTANTIATE_CONVOLUTION(T, float)   \
+    INSTANTIATE_CONVOLUTION(T, double)  \
+
+INSTANTIATE_FOR_TYPE(Pixel)
+INSTANTIATE_FOR_TYPE(float)
+INSTANTIATE_FOR_TYPE(double)
+
+#undef INSTANTIATE_FOR_TYPE
+#undef INSTANTIATE_CONVOLUTION
+
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
--- Libs/Processing/src/Utils.h
+++ Libs/Processing/src/Utils.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 #include <mdspan>
 
 class Layer;
 
 namespace Utils {
 
-    template <typename T>
-    void convolution(Layer& image, std::mdspan<const T, std::dextents<size_t, 2>> kernel);
+    template <typename T, typename U>
+    std::vector<T> convolution(std::mdspan<const T, std::dextents<size_t, 2>> image,
+                               std::mdspan<const U, std::dextents<size_t, 2>> kernel);
 
 }
