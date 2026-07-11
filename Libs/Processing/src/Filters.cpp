9cd0787789e03344e220d55091bd35b6e8747a7d
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-07-11 22:49:25 +0200

    update filters

--- Libs/Processing/src/Filters.cpp
+++ Libs/Processing/src/Filters.cpp
@@ -11,16 +11,49 @@
 void Filters::blur(Layer& image, const int amount) {
     assert(amount > 0);
 
-    const size_t kSize = 2 * amount + 1;
-    const std::vector kernel(kSize, (1.f / kSize));
+    const int32_t kSize = 2 * amount + 1;
+    const std::vector kern(kSize, (1.f / kSize));
 
-    const auto  pixels = std::as_const(image).pixels();
-    std::vector result = Utils::convolution(pixels, std::mdspan(kernel.data(), 1, kernel.size()));
+    struct Op {
+        const Utils::std_mdspan<const float> kernel;
 
-    const auto pixels2 = std::mdspan(std::as_const(result).data(), image.height(), image.width());
-    result             = Utils::convolution(pixels2, std::mdspan(kernel.data(), kernel.size(), 1));
+        std::array<float, 3> init() { return {}; }
 
-    image.setData(std::move(result));
+        void accumulate(std::array<float, 3>& acc, const Pixel pixel, const int32_t ki, const int32_t kj) const {
+            acc[0] += pixel.r * kernel[ki, kj];
+            acc[1] += pixel.g * kernel[ki, kj];
+            acc[2] += pixel.b * kernel[ki, kj];
+        }
+    };
+
+    std::vector clone = image.copyData();
+    std::mdspan src   = std::as_const(image).pixels();
+    std::mdspan dst   = std::mdspan(clone.data(), image.height(), image.width());
+
+    Utils::convolution(src, kSize, 1, Op{ std::mdspan(kern.data(), kern.size(), 1) },
+        [&](const int32_t x, const int32_t y, const std::array<float, 3> sum) {
+            dst[y, x] = {
+                .r = static_cast<uint8_t>(std::clamp(sum[0], 0.f, 255.f)),
+                .g = static_cast<uint8_t>(std::clamp(sum[1], 0.f, 255.f)),
+                .b = static_cast<uint8_t>(std::clamp(sum[2], 0.f, 255.f)),
+                .a = dst[y, x].a
+            };
+        }
+    );
+
+    src = std::mdspan(clone.data(), image.height(), image.width());
+    dst = image.pixels();
+
+    Utils::convolution(src, 1, kSize, Op{ std::mdspan(kern.data(), 1, kern.size()) },
+        [&](const int32_t x, const int32_t y, const std::array<float, 3> sum) {
+            dst[y, x] = {
+                .r = static_cast<uint8_t>(std::clamp(sum[0], 0.f, 255.f)),
+                .g = static_cast<uint8_t>(std::clamp(sum[1], 0.f, 255.f)),
+                .b = static_cast<uint8_t>(std::clamp(sum[2], 0.f, 255.f)),
+                .a = dst[y, x].a
+            };
+        }
+    );
 }
 
 void Filters::swapChannels(Layer& image, const int change) {
@@ -42,79 +75,146 @@ void Filters::swapChannels(Layer& image, const int change) {
 }
 
 void Filters::emboss(Layer& image) {
-    constexpr int32_t kernel[] = {
-        -2, -1, 0,
-        -1,  1, 1,
-         0,  1, 2,
+    constexpr int32_t kernel[] = { -2, -1, 0, /**/ -1,  1, 1, /**/ 0,  1, 2 };
+
+    const std::vector clone    = image.copyData();
+    const std::mdspan src      = std::mdspan(clone.data(), image.height(), image.width());
+    const std::mdspan dst      = image.pixels();
+
+    struct Op {
+        const Utils::std_mdspan<const int32_t> kernel;
+
+        std::array<int32_t, 3> init() { return {}; };
+
+        void accumulate(std::array<int32_t, 3>& acc, const Pixel pixel, const int32_t ki, const int32_t kj) const {
+            acc[0] += pixel.r * kernel[ki, kj];
+            acc[1] += pixel.g * kernel[ki, kj];
+            acc[2] += pixel.b * kernel[ki, kj];
+        }
     };
 
-    const auto  pixels = std::as_const(image).pixels();
-    std::vector result = Utils::convolution(pixels, std::mdspan(kernel, 3, 3));
-
-    image.setData(std::move(result));
+    Utils::convolution(src, 3, 3, Op{ std::mdspan(kernel, 3, 3) },
+        [&](const int32_t x, const int32_t y, const std::array<int32_t, 3> sum) {
+            dst[y, x] = {
+                .r = static_cast<uint8_t>(std::clamp(sum[0], 0, 255)),
+                .g = static_cast<uint8_t>(std::clamp(sum[1], 0, 255)),
+                .b = static_cast<uint8_t>(std::clamp(sum[2], 0, 255)),
+                .a = dst[y, x].a
+            };
+        }
+    );
 }
 
 void Filters::outline(Layer& image) {
-    constexpr int32_t kernel[] = {
-        -1, -1, -1,
-        -1,  8, -1,
-        -1, -1, -1,
-    };
+    constexpr int32_t kernel[] = { -1, -1, -1, /**/ -1,  8, -1, /**/ -1, -1, -1 };
 
     Effects::grayscale(image);
 
-    const auto  pixels = std::as_const(image).pixels();
-    std::vector result = Utils::convolution(pixels, std::mdspan(kernel, 3, 3));
+    const std::vector clone = image.copyData();
+    const std::mdspan src   = std::mdspan(clone.data(), image.height(), image.width());
+    const std::mdspan dst   = image.pixels();
 
-    image.setData(std::move(result));
+    struct Op {
+        const Utils::std_mdspan<const int32_t> kernel;
+
+        std::array<int32_t, 3> init() { return {}; };
+
+        void accumulate(std::array<int32_t, 3>& acc, const Pixel pixel, const int32_t ki, const int32_t kj) const {
+            acc[0] += pixel.r * kernel[ki, kj];
+            acc[1] += pixel.g * kernel[ki, kj];
+            acc[2] += pixel.b * kernel[ki, kj];
+        }
+    };
+
+    Utils::convolution(src, 3, 3, Op{ std::mdspan(kernel, 3, 3) },
+        [&](const int32_t x, const int32_t y, const std::array<int32_t, 3> sum) {
+            dst[y, x] = {
+                .r = static_cast<uint8_t>(std::clamp(sum[0], 0, 255)),
+                .g = static_cast<uint8_t>(std::clamp(sum[1], 0, 255)),
+                .b = static_cast<uint8_t>(std::clamp(sum[2], 0, 255)),
+                .a = dst[y, x].a
+            };
+        }
+    );
 }
 
 void Filters::laplace(Layer& image) {
-    constexpr int32_t ker[] = { 1, 4, 1, /**/ 4, -20, 4, /**/ 1, 4, 1 };
+    constexpr int32_t kernel[] = { 1, 4, 1, /**/ 4, -20, 4, /**/ 1, 4, 1 };
 
     Filters::blur(image, 1);
     Effects::grayscale(image);
 
     const std::vector promoted = Utils::promote<int32_t>(image.data());
-    const std::mdspan pixels   = std::mdspan(promoted.data(), image.height(), image.width());
-    const std::span   data     = image.data();
+    const std::mdspan src      = std::mdspan(promoted.data(), image.height(), image.width());
+    const std::mdspan dst      = image.pixels();
 
-    const std::vector L        = Utils::convolution(pixels, std::mdspan(ker, 3, 3));
+    struct LaplaceOp {
+        const Utils::std_mdspan<const int32_t> kernel;
 
-    for (size_t i = 0; i < data.size(); ++i) {
-        const uint8_t G = static_cast<uint8_t>(std::clamp(std::abs(L[i]), 0, 255));
-        data[i] = {
-            .r = G,
-            .g = G,
-            .b = G,
-            .a = data[i].a
-        };
-    }
+        int32_t init() { return 0; }
+
+        void accumulate(int32_t& acc, const int32_t pixel, const int32_t ki, const int32_t kj) const {
+            acc += pixel * kernel[ki, kj];
+        }
+    };
+
+    Utils::convolution(src, 3, 3, LaplaceOp{ std::mdspan(kernel, 3, 3) },
+        [&](const int32_t x, const int32_t y, const int32_t sum) {
+            const uint8_t G = static_cast<uint8_t>(std::clamp(std::abs(sum), 0, 255));
+            dst[y, x] = {
+                .r = G,
+                .g = G,
+                .b = G,
+                .a = dst[y, x].a
+            };
+        }
+    );
 }
 
 namespace {
 
-void edgeDetectionHelper(Layer& image, const int32_t dx[], const int32_t dy[]) {
+template <typename T>
+using std_mdspan = std::mdspan<T, std::dextents<size_t, 2>>;
+
+void edgeDetectionHelper(Layer& image, const std_mdspan<const int32_t> dx, const std_mdspan<const int32_t> dy) {
     Filters::blur(image, 1);
     Effects::grayscale(image);
 
     const std::vector promoted = Utils::promote<int32_t>(image.data());
-    const std::mdspan pixels   = std::mdspan(promoted.data(), image.height(), image.width());
-    const std::span   data     = image.data();
+    const std::mdspan src      = std::mdspan(promoted.data(), image.height(), image.width());
+    const std::mdspan dst      = image.pixels();
 
-    const std::vector Gx       = Utils::convolution(pixels, std::mdspan(dx, 3, 3));
-    const std::vector Gy       = Utils::convolution(pixels, std::mdspan(dy, 3, 3));
+    struct State {
+        int32_t sumX, sumY;
+    };
 
-    for (size_t i = 0; i < data.size(); ++i) {
-        const int32_t G = std::abs(Gx[i]) + std::abs(Gy[i]);
-        const uint8_t v = static_cast<uint8_t>(std::clamp(G, 0, 255));
-        data[i] = {
-            .r = v,
-            .g = v,
-            .b = v,
-            .a = data[i].a
-        };
-    }
+    struct GradientOp {
+        const std_mdspan<const int32_t> dx;
+        const std_mdspan<const int32_t> dy;
+
+        State init() {
+            return { 0, 0 };
+        }
+
+        void accumulate(State& state, const int32_t pixel, const int32_t ki, const int32_t kj) const {
+            state.sumX += pixel * dx[ki, kj];
+            state.sumY += pixel * dy[ki, kj];
+        }
+    };
+
+    Utils::convolution(src, 3, 3, GradientOp{ dx, dy },
+        [&](const int32_t x, const int32_t y, const State& state) {
+            const int32_t G = std::abs(state.sumX) + std::abs(state.sumY);
+            const uint8_t v = static_cast<uint8_t>(std::clamp(G, 0, 255));
+
+            dst[y, x] = {
+                .r = v,
+                .g = v,
+                .b = v,
+                .a = dst[y, x].a
+            };
+        }
+    );
 }
 
 }
@@ -122,17 +222,17 @@ void edgeDetectionHelper(Layer& image, const int32_t dx[], const int32_t dy[]) {
 void Filters::prewitt(Layer& image) {
     constexpr int32_t dx[] = { -1,  0,  1, /**/ -1, 0, 1, /**/ -1, 0, 1 };
     constexpr int32_t dy[] = { -1, -1, -1, /**/  0, 0, 0, /**/  1, 1, 1 };
-    ::edgeDetectionHelper(image, dx, dy);
+    ::edgeDetectionHelper(image, std::mdspan(dx, 3, 3), std::mdspan(dy, 3, 3));
 }
 
 void Filters::scharr(Layer& image) {
     constexpr int32_t dx[] = { -3,   0,  3, /**/ -10, 0, 10, /**/ -3,  0, 3 };
     constexpr int32_t dy[] = { -3, -10, -3, /**/   0, 0,  0, /**/  3, 10, 3 };
-    ::edgeDetectionHelper(image, dx, dy);
+    ::edgeDetectionHelper(image, std::mdspan(dx, 3, 3), std::mdspan(dy, 3, 3));
 }
 
 void Filters::sobel(Layer& image) {
     constexpr int32_t dx[] = { -1, 0, 1, /**/ -2, 0, 2, /**/ -1,  0,  1 };
     constexpr int32_t dy[] = {  1, 2, 1, /**/  0, 0, 0, /**/ -1, -2, -1 };
-    ::edgeDetectionHelper(image, dx, dy);
+    ::edgeDetectionHelper(image, std::mdspan(dx, 3, 3), std::mdspan(dy, 3, 3));
 }
