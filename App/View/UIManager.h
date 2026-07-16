7ee67abd5a2c7b9db0f724dd8fd228e20d76867f
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-07-16 20:53:37 +0200

    Update UIManager.h

--- App/View/UIManager.h
+++ App/View/UIManager.h
@@ -2,10 +2,18 @@
 
 #include <Assets/AssetManager.h>
 #include <functional>
+#include <optional>
+#include <string>
 
 class Renderer;
 class EditorState;
 class Controller;
+
+struct ActivePopup {
+    std::string_view      title;
+    std::function<void()> drawContent;
+    std::function<void()> onApply;
+};
 
 class UIManager {
 public:
@@ -20,10 +28,14 @@ private:
     uint8_t      m_theme    = 0;
 
 private:
+    std::optional<ActivePopup> m_activePopup;
+
+private:
     void drawToolbar(EditorState& state);
     void drawPropertiesPanel(EditorState& state);
     void drawMenuBar(EditorState& state, Controller& controller, const std::function<void()>& onQuitRequest);
     void drawCanvas(EditorState& state);
+    void drawPendingPopups();
 
     void setTheme(uint8_t theme);
 };
