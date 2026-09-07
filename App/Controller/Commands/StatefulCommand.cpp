f6fdf5fdad6c9526d3c1fbbe32ff8592f8380c92
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-09-07 22:06:45 +0200

    create StatefulCommand

---
+++ App/Controller/Commands/StatefulCommand.cpp
@@ -0,0 +1,5 @@
+#include "StatefulCommand.h"
+
+namespace Cmd {
+
+}
---
+++ App/Controller/Commands/StatefulCommand.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Controller/Command.h>
+#include <functional>
+
+namespace Cmd {
+
+    template <class State>
+    class StatefulCommand final : public Command {
+    public:
+        using Function = std::move_only_function<void(EditorState&, State&)>;
+
+        StatefulCommand(const CommandNames name, State state, Function execute, Function undo)
+            : Command(name), m_state(std::move(state)), m_execute(std::move(execute)), m_undo(std::move(undo)) {}
+
+        void execute(EditorState& state) override {
+            m_execute(state, m_state);
+        }
+
+        void undo(EditorState& state) override {
+            m_undo(state, m_state);
+        }
+
+    private:
+        State    m_state;
+        Function m_execute;
+        Function m_undo;
+    };
+
+}
