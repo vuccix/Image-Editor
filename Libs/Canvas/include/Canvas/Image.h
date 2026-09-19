a9ec983b3845821886af44d1593db5d1f1974e56
Vuk Djukanović <89420333+vuccix@users.noreply.github.com>
2026-09-19 23:05:40 +0200

    change Image to store vector of Pixels

--- Libs/Canvas/include/Canvas/Image.h
+++ Libs/Canvas/include/Canvas/Image.h
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <Canvas/Pixel.h>
 #include <cstdint>
 #include <vector>
 
 struct Image {
-    std::vector<uint8_t> pixels;
-    uint32_t             width  = 0;
-    uint32_t             height = 0;
+    std::vector<Pixel> pixels;
+    uint32_t           width  = 0;
+    uint32_t           height = 0;
 };
